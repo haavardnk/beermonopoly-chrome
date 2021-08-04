381a8e77fe70f92a6bd553b86ff8fd848d0ebba7
Håvard Kråkenes <haavardnk@outlook.com>
2021-08-04 18:59:12 +0200

    Fix bug where Untappd info sometimes would not load in detail view

--- details.js
+++ details.js
@@ -5,98 +5,108 @@ async function getBeer(beer_id) {
     return data;
 }
 
-if (document.getElementsByClassName("product__category-name")[0].innerText.includes("ØL")) {
-    // Get beer information
-    var beer_id = document.getElementsByClassName("product__tab-list")[0]
-        .getElementsByTagName("li")[1]
-        .getElementsByTagName("span")[1].innerText;
+function main() {
+    if (document.getElementsByClassName("product__category-name")[0].innerText.includes("ØL")) {
 
-    // Construct HTML
-    var untappd = document.createElement("div");
-    var logo = document.createElement("img");
-    var link = document.createElement("a");
-    var updated = document.createElement("p");
-    var wrong = document.createElement("a");
+        // Get beer id from DOM
+        var beer_id = document.getElementsByClassName("product__tab-list")[0]
+            .getElementsByTagName("li")[1]
+            .getElementsByTagName("span")[1].innerText;
 
-    untappd.classList.add("untappd");
-    wrong.classList.add("suggest");
-    logo.src = chrome.runtime.getURL("assets/img/untappd.svg");
+        // Construct HTML
+        var untappd = document.createElement("div");
+        var logo = document.createElement("img");
+        var link = document.createElement("a");
+        var updated = document.createElement("p");
+        var wrong = document.createElement("a");
 
-    untappd.appendChild(logo);
-    untappd.appendChild(link);
-    untappd.appendChild(updated);
-    untappd.appendChild(wrong);
+        untappd.classList.add("untappd");
+        wrong.classList.add("suggest");
+        logo.src = chrome.runtime.getURL("assets/img/untappd.svg");
 
-    document.getElementsByClassName('product__layout-wrapper')[0].appendChild(untappd);
+        untappd.appendChild(logo);
+        untappd.appendChild(link);
+        untappd.appendChild(updated);
+        untappd.appendChild(wrong);
 
-    getBeer(beer_id).then(function (data) {
-        if (data.rating !== null) {
-            var date = new Date(data.untpd_updated);
-            link.href = data.untpd_url;
-            link.innerText = data.rating.toPrecision(3) + " (" + data.checkins + ")";
-            updated.innerText = "Oppdatert: " + date.toLocaleDateString() + " " + date.toLocaleTimeString();
-            wrong.innerText = "Feil øl?";
-        }
-        else {
-            link.innerText = "Ingen match";
-            wrong.innerText = "Foreslå Untappd match";
-        }
+        document.getElementsByClassName('product__layout-wrapper')[0].appendChild(untappd);
 
-    });
+        // Get beer info
+        getBeer(beer_id).then(function (data) {
+            if (data.rating !== null) {
+                var date = new Date(data.untpd_updated);
+                link.href = data.untpd_url;
+                link.innerText = data.rating.toPrecision(3) + " (" + data.checkins + ")";
+                updated.innerText = "Oppdatert: " + date.toLocaleDateString('en-GB') + " " + date.toLocaleTimeString('en-GB');
+                wrong.innerText = "Feil øl?";
+            }
+            else {
+                link.innerText = "Ingen match";
+                wrong.innerText = "Foreslå Untappd match";
+            }
 
-    wrong.addEventListener("click", function (e) {
-        e.preventDefault();
-        Swal.fire({
-            title: 'Rapporter feil Untappd match',
-            text: 'Legg inn riktig Untappd link. Eksempel: https://untappd.com/b/nogne-o-porter/27638',
-            input: 'text',
-            inputAttributes: {
-                autocapitalize: 'off'
-            },
-            showCancelButton: false,
-            confirmButtonText: 'Send',
-            confirmButtonColor: '#002025',
-        }).then((result) => {
-            if (result.isConfirmed && result.value) {
-                const data = JSON.stringify({ "beer": beer_id, "suggested_url": result.value })
-                const xhr = new XMLHttpRequest();
-                const baseUrl = "https://api.beermonopoly.com/wrongmatch/";
+        });
 
-                xhr.open("POST", baseUrl, true);
-                xhr.setRequestHeader("Content-type", "application/json");
-                xhr.send(data);
+        // Report wrong match modal
+        wrong.addEventListener("click", function (e) {
+            e.preventDefault();
+            Swal.fire({
+                title: 'Rapporter feil Untappd match',
+                text: 'Legg inn riktig Untappd link. Eksempel: https://untappd.com/b/nogne-o-porter/27638',
+                input: 'text',
+                inputAttributes: {
+                    autocapitalize: 'off'
+                },
+                showCancelButton: false,
+                confirmButtonText: 'Send',
+                confirmButtonColor: '#002025',
+            }).then((result) => {
+                if (result.isConfirmed && result.value) {
+                    const data = JSON.stringify({ "beer": beer_id, "suggested_url": result.value })
+                    const xhr = new XMLHttpRequest();
+                    const baseUrl = "https://api.beermonopoly.com/wrongmatch/";
 
-                xhr.onreadystatechange = function () { // Call a function when the state changes.
-                    console.log(xhr.status)
-                    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 201) {
-                        Swal.fire({
-                            title: 'Feil registrert!',
-                            text: 'Ditt endringsforslag vil bli evaluert. Takk for hjelpen!',
-                            icon: 'success',
-                            confirmButtonColor: '#002025',
-                        });
+                    xhr.open("POST", baseUrl, true);
+                    xhr.setRequestHeader("Content-type", "application/json");
+                    xhr.send(data);
+
+                    xhr.onreadystatechange = function () { // Call a function when the state changes.
+                        console.log(xhr.status)
+                        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 201) {
+                            Swal.fire({
+                                title: 'Feil registrert!',
+                                text: 'Ditt endringsforslag vil bli evaluert. Takk for hjelpen!',
+                                icon: 'success',
+                                confirmButtonColor: '#002025',
+                            });
+                        }
+                        else {
+                            Swal.fire({
+                                title: 'Det oppsto en feil ved sending av forslaget...',
+                                text: 'Sjekk at du har tastet inn en gyldig URL!',
+                                icon: 'error',
+                                confirmButtonColor: '#002025',
+                            });
+                        }
+
                     }
-                    else {
-                        Swal.fire({
-                            title: 'Det oppsto en feil ved sending av forslaget...',
-                            text: 'Sjekk at du har tastet inn en gyldig URL!',
-                            icon: 'error',
-                            confirmButtonColor: '#002025',
-                        });
-                    }
+                }
+                else if (result.isConfirmed && !result.value) {
+                    Swal.fire({
+                        title: 'Du må oppgi en gyldig Untappd link!',
+                        text: 'Eksempel: https://untappd.com/b/nogne-o-porter/27638',
+                        icon: 'error',
+                        confirmButtonColor: '#002025',
+                    });
 
                 }
-            }
-            else if (result.isConfirmed && !result.value) {
-                Swal.fire({
-                    title: 'Du må oppgi en gyldig Untappd link!',
-                    text: 'Eksempel: https://untappd.com/b/nogne-o-porter/27638',
-                    icon: 'error',
-                    confirmButtonColor: '#002025',
-                });
-
-            }
-        })
-    });
-
+            })
+        });
+    }
 }
+
+// Wait for Vinmonopolet to load beer
+document.arrive(".product__price", function () {
+    main();
+    Arrive.unbindAllArrive();
+});
