4757e5d56529179ffd4e22195f7f25b37778c3d5
Håvard Kråkenes <haavardnk@outlook.com>
2021-09-21 10:14:30 +0200

    Add sentry error logging

--- popup-script.js
+++ popup-script.js
@@ -5,3 +5,7 @@ document.querySelector('#sign-in')
             if (response === 'success') window.close();
         });
     });
+Sentry.init({
+    dsn: "https://72b452d00a6b4c8a820c6f122cd717a2@o985007.ingest.sentry.io/5970536",
+    tracesSampleRate: 0.2,
+});
